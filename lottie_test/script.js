087d54b7854ab0eb886edd488236de156dfcedca
wuwindy <welfare34@hotmail.com>
2022-09-07 11:20:42 +0800

    lottie_test

---
+++ lottie_test/script.js
@@ -0,0 +1,18 @@
+const anlottie = lottie.loadAnimation({
+  container: svgContainer,
+  animType: 'svg',
+  loop: true,
+  autoplay: false,
+  path: 'https://assets4.lottiefiles.com/packages/lf20_Sjy4Vl.json',
+});
+  
+// anlottie.setSpeed(10);
+anlottie.play();
+
+// function play() {
+//   anlottie.play();
+// }
+
+// function stop() {
+//   anlottie.pause();
+// }
