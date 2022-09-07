ea56f4bcca5f2b64d1219d8f5e18a82cf1de7438
wuwindy <welfare34@hotmail.com>
2022-09-07 11:25:38 +0800

    Update script.js

--- lottie_test/script.js
+++ lottie_test/script.js
@@ -3,7 +3,7 @@ const anlottie = lottie.loadAnimation({
   animType: 'svg',
   loop: true,
   autoplay: false,
-  path: 'https://assets4.lottiefiles.com/packages/lf20_Sjy4Vl.json',
+  path: 'data.json',
 });
   
 // anlottie.setSpeed(10);
